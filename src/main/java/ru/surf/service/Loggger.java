9e5910f9e5c40020d083211536667d4dc2e0d4f1
Aleksandr Riazantsev <ad.riazantsev@gmail.com>
2024-06-20 20:58:15 +0300

    fix(Loggger): fix duplicating logger messages

--- src/main/java/ru/surf/service/Loggger.java
+++ src/main/java/ru/surf/service/Loggger.java
@@ -3,68 +3,25 @@ package ru.surf.service;
 import java.util.logging.*;
 
 public class Loggger {
-    private Logger logger;
-    private ConsoleHandler consoleHandler;
-    
-    private Loggger() {
-        logger = Logger.getLogger("Loggger");
-        consoleHandler = new ConsoleHandler();
-        
-        try {
-            consoleHandler.setEncoding("UTF-8");
-            logger.addHandler(consoleHandler);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static Loggger instance = new Loggger();
+    private static final Logger logger = Logger.getLogger(Loggger.class.getName());
+    private static Loggger instance;
 
     public static Loggger getInstance() {
+        if (instance == null) {
+            instance = new Loggger();
+        }
         return instance;
     }
 
-    public void logInfo(String message) {
+    public static void logInfo(String message) {
         logger.log(Level.INFO, message);
     }
 
-    public void logError(String message) {
+    public static void logWarning(String message) {
+        logger.log(Level.WARNING, message);
+    }
+
+    public static void logError(String message) {
         logger.log(Level.SEVERE, message);
-    }    
+    }
 }
-
-// package ru.surf.utils;
-
-// import java.util.logging.*;
-
-// public class Loggger {
-//     private static Logger logger = Logger.getLogger("Loggger");
-//     private static Loggger instance;
-//     private ConsoleHandler consoleHandler;
-    
-//     private Loggger() {
-//         consoleHandler =  new ConsoleHandler();
-        
-//         try {
-//             consoleHandler.setEncoding("UTF-8");
-//             logger.addHandler(consoleHandler);
-//         } catch (Exception e) {
-//             e.printStackTrace();
-//         }
-//     }
-
-//     public static Loggger getInstance() {
-//         if (instance == null) {
-//             instance = new Loggger();
-//         }
-//         return instance;
-//     }
-
-//     public static void logInfo(String message) {
-//         logger.log(Level.INFO, message);
-//     }
-
-//     public static void logError(String message) {
-//         logger.log(Level.SEVERE, message);
-//     }    
-// }
